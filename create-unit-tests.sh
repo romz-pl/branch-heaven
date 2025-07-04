285cac761f9896200837e7d5140e9a6d74e024ae
romz <romz@wp.pl>
2025-07-04 19:46:10 +0200

    Crete the first version of the script

---
+++ create-unit-tests.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+
+get_prompt() {
+    local path="$1"
+    echo "Generate unit test for file ${path}"
+}
+
+get_branch() {
+    local path="$1"
+    echo "cl-T$(uuidgen)"
+}
+
+
+run_claude_code() {
+    local path="$1"
+    local branch=$(get_branch)
+    echo $branch
+    local prompt=$(get_prompt $path)
+    echo $prompt
+
+    git checkout main
+    git pull
+    git checkout -b $branch
+
+    echo $path > log.txt
+    sleep 5s
+
+    git add .
+    git commit -m "Add unit test for file $path"
+    git push
+}
+
+run_claude_code "path/to/file-1.java"
+run_claude_code "path/to/file-2.java"
